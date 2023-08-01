e84778ea0b82e3d48ade910a1c8087cd81c7a34b
Thijs Nissen <thijsnissen@MacBook-Pro-van-Thijs.local>
2023-08-01 15:19:10 +0200

    fixes

--- utilities/src/main/scala/Box.scala
+++ utilities/src/main/scala/Box.scala
@@ -18,10 +18,6 @@ case class Box(min: Pos, max: Pos):
 		yield
 			Pos(x, y)
 
-	def asString: String =
-		val box = max - min
-		Vector.fill(box.x, box.y)('.').map(row => row.map(_.toString).mkString("\n", " ", "")).mkString
-
 object Box:
 	def apply(p: Pos): Box = Box(p, p)
 
