09686088f7b033c633bc6246263f1e3d2559d16a
Thijs Nissen <thijsnissen@MacBook-Pro-van-Thijs.local>
2023-08-02 13:15:10 +0200

    finished day 16

--- 2018/src/test/scala/Test.scala
+++ 2018/src/test/scala/Test.scala
@@ -70,6 +70,14 @@ class Test extends AnyFunSuite:
 			assertResult("103,85")(Day13.pt1) // [4ms]
 			assertResult("88,64")(Day13.pt2) // [20ms]
 
+	test("Day16"):
+		if Day16.given_Mode == Day16.Mode.Test then
+			assertResult(1)(Day16.pt1) // [1ms]
+
+		if Day16.given_Mode == Day16.Mode.Prod then
+			assertResult(544)(Day16.pt1) // [7ms]
+			assertResult(600)(Day16.pt2) // [4ms]
+
 //import scala.io.Source
 //
 //object Day00 extends App:
