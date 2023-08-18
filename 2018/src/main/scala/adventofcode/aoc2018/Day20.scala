6083af2396995dfc59314b0fac9cd898060a5ee5
Thijs Nissen <thijsnissen@MacBook-Pro-van-Thijs.local>
2023-08-18 10:59:07 +0200

    update with comments of JP

--- 2018/src/main/scala/adventofcode/aoc2018/Day20.scala
+++ 2018/src/main/scala/adventofcode/aoc2018/Day20.scala
@@ -65,15 +65,21 @@ object Day20 extends AdventOfCode:
 			case 'E' => p + Pos(1, 0)
 			case 'W' => p + Pos(-1, 0)
 
+	// We turn each direction/character into a Pos(x, y) and a corresponding
+	// total travel distance so far.
 	def offsetRooms(rooms: Vector[Char], pos: Pos, dist: Int): Vector[(Pos, Int)] =
 		rooms.foldLeft(Vector((pos, dist))):
 			(a, c) =>
 				val (p, d) = a.last
 				a :+ (offset(p, c), d + 1)
 
-	def instructionsToMap(instrutctions: Vector[Instructions]): Map[Pos, Int] =
+	def instructionsToFloorplan(instructions: Vector[Instructions]): Map[Pos, Int] =
 		def loop(instr: Vector[Instructions], currPos: Pos, currDist: Int, acc: Vector[(Pos, Int)]): Vector[(Pos, Int)] =
 			instr.headOption match
+				// Once we encounter a seqence of rooms, the deepest level of the Instructions
+				// datastructure, we get for each room it's Pos and the distance travelled up to that room.
+				// We add them to the accumulator and set the currPos and currDist to the furthest
+				// we've gone so far. Continue with the next instruction (instr.tail).
 				case Some(Rooms(get)) =>
 					val rooms =
 						offsetRooms(get, currPos, currDist)
@@ -82,22 +88,45 @@ object Day20 extends AdventOfCode:
 						rooms.maxBy((_, d) => d)
 
 					loop(instr.tail, maxPos, maxDist, acc ++ rooms.tail)
+
+				// A sequence of rooms that lead to a dead end and are then travelled again
+				// backwards (SSEE WWNN). Because of that we only have the index half of the
+				// route. Since a RoomsDetour leads back to the point where it started we
+				// continue to the next instruction (instr.tail) with the currPos and
+				// currDist unchanged.
 				case Some(RoomsDetour(get)) =>
 					val rooms =
 						offsetRooms(get.take(get.length / 2), currPos, currDist)
 
 					loop(instr.tail, currPos, currDist, acc ++ rooms.tail)
+
+				// A Branch consists of multiple leafs. All we need to do is add them to
+				// the queue for further processing from the currPos and currDist.
 				case Some(Branch(get)) =>
 					loop(instr.tail ++ get, currPos, currDist, acc)
+
+				// A leaf is series of instructions that need to be analyzed separately
+				// from the currPos and currDist. It is basically being able to turn left
+				// and right on an intersection. Each direction has to be explored separately
+				// from the currPos and currDist. Once a leaf (i.e. pick a direction) is
+				// fully explored, it's result is added to the accumulator and we continue
+				// to the next instruction (instr.tail) from the point of the intersection
+				// (currPos and currDist).
 				case Some(Leaf(get)) =>
 					val leaf =
 						loop(get, currPos, currDist, Vector.empty[(Pos, Int)])
 
 					loop(instr.tail, currPos, currDist, acc ++ leaf)
+
+				// When all the instructions are processed we return the accumulator.
 				case None =>
 					acc
 
-		loop(instrutctions, Pos.unit, 0, Vector.empty[(Pos, Int)])
+		// In the full puzzle input some rooms are visited multiple times. This
+		// apparently happens not only in the roomsDetour (because there we account
+		// for it) but also in the main route. With groupMapReduce we pick the shortest
+		// distance for every Pos and remove the rest.
+		loop(instructions, Pos.unit, 0, Vector.empty[(Pos, Int)])
 			.groupMapReduce((p, _) => p)((_, i) => i)((a, b) => a min b)
 
 	def parse(r: String): Vector[Instructions] =
@@ -107,7 +136,7 @@ object Day20 extends AdventOfCode:
 
 	lazy val pt1 =
 		val result =
-			instructionsToMap(parse(regex))
+			instructionsToFloorplan(parse(regex))
 
 		val (_, maxDistance) =
 			result.maxBy((_, d) => d)
@@ -115,10 +144,8 @@ object Day20 extends AdventOfCode:
 		maxDistance
 
 	lazy val pt2 =
-		val result =
-			instructionsToMap(parse(regex))
-
-		result.count((_, i) => i >= 1000)
+		instructionsToFloorplan(parse(regex))
+			.count((_, i) => i >= 1000)
 
 	answer(1)(pt1)
 
