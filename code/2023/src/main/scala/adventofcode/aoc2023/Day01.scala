87b478c5205f6c8ce952d88e60fdbeaa989a6ecf
Thijs Nissen <thijsnissen@gmail.com>
2023-12-01 21:40:13 +0100

    finished 2023 day01

--- code/2023/src/main/scala/adventofcode/aoc2023/Day01.scala
+++ code/2023/src/main/scala/adventofcode/aoc2023/Day01.scala
@@ -4,14 +4,45 @@ package aoc2023
 import utilities.AdventOfCode.*
 
 object Day01 extends AdventOfCode(Prod):
-  val myInput =
+  val calibrationDocument: Vector[String] =
     input
+      .linesIterator
+      .toVector
+
+  val digits: Map[String, String] =
+    Map(
+      "one"   -> "o1e",
+      "two"   -> "t2o",
+      "three" -> "t3e",
+      "four"  -> "f4r",
+      "five"  -> "f5e",
+      "six"   -> "s6x",
+      "seven" -> "s7n",
+      "eight" -> "e8t",
+      "nine"  -> "n9e",
+    )
+
+  @tailrec
+  def firstValidDigit(line: String): Int =
+    if line.head.isDigit then line.head.asDigit else firstValidDigit(line.tail)
+
+  def normalize(line: String): String =
+    digits.foldLeft(line):
+      case (acc, (a, b)) => acc.replaceAll(a, b)
+
+  def calibrationValue(line: String): Int =
+    s"${firstValidDigit(line)}${firstValidDigit(line.reverse)}".toInt
 
   lazy val pt1: Int =
-    ???
+    calibrationDocument
+      .map(calibrationValue)
+      .sum
 
   lazy val pt2: Int =
-    ???
+    calibrationDocument
+      .map(normalize)
+      .map(calibrationValue)
+      .sum
 
   answer(1)(pt1)
 
--- code/2023/src/test/scala/adventofcode/aoc2023/AdventOfCode2023Spec.scala
+++ code/2023/src/test/scala/adventofcode/aoc2023/AdventOfCode2023Spec.scala
@@ -7,9 +7,9 @@ import utilities.AdventOfCode.Env.*
 class AdventOfCode2023Spec extends AnyFunSuite:
   test("Day01"):
     if Day01.env == Test then
-      assertResult(???)(Day01.pt1) // [0ms]
-      assertResult(???)(Day01.pt2) // [0ms]
+      assertResult(142)(Day01.pt1) // [2ms]
+      assertResult(225)(Day01.pt2) // [2ms]
 
     if Day01.env == Prod then
-      assertResult(???)(Day01.pt1) // [0ms]
-      assertResult(???)(Day01.pt2) // [0ms]
+      assertResult(56397)(Day01.pt1) // [5ms]
+      assertResult(55701)(Day01.pt2) // [18ms]
