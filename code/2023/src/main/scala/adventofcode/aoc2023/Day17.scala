26a954c03b90413817a733f8119a72a2e9e65fcb
Thijs Nissen <thijsnissen@gmail.com>
2023-12-17 12:46:48 +0100

    cleanup

--- code/2023/src/main/scala/adventofcode/aoc2023/Day17.scala
+++ code/2023/src/main/scala/adventofcode/aoc2023/Day17.scala
@@ -47,23 +47,23 @@ object Day17 extends AdventOfCode(Prod):
         costs.sum - self(sX)(sY)
 
       def toGraph(
-        offsetFrom: Int,
-        offsetTo: Int
+        minOffset: Int,
+        maxOffset: Int
       ): WeightedGraph[(Int, Int, Direction)] =
         val (xMax: Int, yMax: Int) = self.size
 
         WeightedGraph.unit: (sX: Int, sY: Int, sDir: Direction) =>
           lazy val horizontal: IndexedSeq[(Int, Int, Direction)] =
-            (offsetFrom to offsetTo).flatMap: (i: Int) =>
+            (minOffset to maxOffset).flatMap: (i: Int) =>
               Seq((sX + i, sY, Horizontal), (sX - i, sY, Horizontal))
 
           lazy val vertical: IndexedSeq[(Int, Int, Direction)] =
-            (offsetFrom to offsetTo).flatMap: (i: Int) =>
+            (minOffset to maxOffset).flatMap: (i: Int) =>
               Seq((sX, sY + i, Vertical), (sX, sY - i, Vertical))
 
           val next: IndexedSeq[(Int, Int, Direction)] = sDir match
-            case Vertical   => horizontal
             case Horizontal => vertical
+            case Vertical   => horizontal
             case Unknown    => horizontal ++ vertical
 
           next
@@ -81,18 +81,16 @@ object Day17 extends AdventOfCode(Prod):
   lazy val pt1: Int =
     val (xMax: Int, yMax: Int) = trafficPatterns.size
 
-    val graph: WeightedGraph[(Int, Int, Direction)] =
-      trafficPatterns.toGraph(1, 3)
-
-    graph.leastHeatlossTo(xMax, yMax)
+    trafficPatterns
+      .toGraph(1, 3)
+      .leastHeatlossTo(xMax, yMax)
 
   lazy val pt2: Int =
     val (xMax: Int, yMax: Int) = trafficPatterns.size
 
-    val graph: WeightedGraph[(Int, Int, Direction)] =
-      trafficPatterns.toGraph(4, 10)
-
-    graph.leastHeatlossTo(xMax, yMax)
+    trafficPatterns
+      .toGraph(4, 10)
+      .leastHeatlossTo(xMax, yMax)
 
   answer(1)(pt1)
 
