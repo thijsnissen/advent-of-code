6b86aae74209f2db035e6133ec2bd953660c1cf6
Thijs Nissen <thijsnissen@gmail.com>
2024-12-04 08:44:27 +0100

    Finished 2024 day 4 (#58)

---
+++ code/2024/src/main/scala/adventofcode/aoc2024/Day04.scala
@@ -0,0 +1,104 @@
+package adventofcode
+package aoc2024
+
+import adventofcode.utilities.AdventOfCode.*
+import adventofcode.utilities.Pos
+
+object Day04 extends AdventOfCode(Prod):
+  val wordSearch: WordSearch =
+    (for
+      (l, y) <- input.linesIterator.zipWithIndex
+      (c, x) <- l.zipWithIndex
+    yield Pos(x, y) -> c).toMap
+
+  enum Direction:
+    case Hor, Ver, DgL, DgR
+
+  object Direction:
+    extension (self: Direction)
+      def offset(pos: Pos)(f: Pos => Boolean): Set[Pos] =
+        val offset =
+          self match
+            case Hor => Set(
+                pos.copy(x = pos.x - 1),
+                pos.copy(x = pos.x + 1)
+              )
+            case Ver => Set(
+                pos.copy(y = pos.y - 1),
+                pos.copy(y = pos.y + 1)
+              )
+            case DgL => Set(
+                pos.copy(x = pos.x - 1, y = pos.y + 1),
+                pos.copy(x = pos.x + 1, y = pos.y - 1)
+              )
+            case DgR => Set(
+                pos.copy(x = pos.x - 1, y = pos.y - 1),
+                pos.copy(x = pos.x + 1, y = pos.y + 1)
+              )
+
+        offset.filter(f)
+
+  type WordSearch = Map[Pos, Char]
+
+  object WordSearch:
+    extension (self: WordSearch)
+      def allPos(char: Char): Set[Pos] =
+        self
+          .filter((_, c) => c == char)
+          .keys
+          .toSet
+
+      def findXmas: Int =
+        @tailrec def loop(xs: Vector[(Pos, Direction)], acc: Int = 0): Int =
+          xs.headOption match
+            case None => acc
+            case Some(x, dir) =>
+              val s =
+                for
+                  m <- dir.offset(x)(self.get(_).contains('M'))
+                  a <- dir.offset(m)(self.get(_).contains('A'))
+                yield dir.offset(a)(self.get(_).contains('S')).size
+
+              loop(xs.tail, acc + s.sum)
+
+        val xs =
+          self.allPos('X').toVector
+
+        loop(
+          xs.map(_ -> Direction.Hor) ++ xs.map(_ -> Direction.Ver) ++
+            xs.map(_ -> Direction.DgL) ++ xs.map(_ -> Direction.DgR)
+        )
+
+      def findXedMas: Int =
+        def isMas(p: Pos)(d: Direction): Boolean =
+          (d.offset(p)(_ => true) + p)
+            .flatMap(self.get)
+            .intersect(Set('M', 'A', 'S'))
+            .size == 3
+
+        @tailrec def loop(ps: Vector[Pos], acc: Int = 0): Int =
+          ps.headOption match
+            case None => acc
+            case Some(p) =>
+              loop(
+                ps.tail,
+                if
+                  isMas(p)(Direction.DgR) &&
+                  isMas(p)(Direction.DgL)
+                then acc + 1
+                else acc
+              )
+
+        loop(self.allPos('A').toVector)
+
+  import WordSearch.*
+
+  lazy val pt1: Int =
+    wordSearch.findXmas
+
+  lazy val pt2: Int =
+    wordSearch.findXedMas
+
+  answer(1)(pt1)
+
+  answer(2)(pt2)
