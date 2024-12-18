31a1a833a8dc500b14bece9377c11b8cc2b59a02
Thijs Nissen <thijsnissen@gmail.com>
2024-12-18 12:59:43 +0100

    finished 2024 day 18

---
+++ code/2024/src/main/scala/adventofcode/aoc2024/Day18.scala
@@ -0,0 +1,83 @@
+package adventofcode
+package aoc2024
+
+import utilities.AdventOfCode.*
+import utilities.Box
+import utilities.Pos
+import utilities.Utilities.exponentialBinarySearch
+
+object Day18 extends AdventOfCode(Prod):
+  val bytes: Bytes =
+    Bytes.fromString(input)
+
+  type Bytes = Vector[Pos]
+
+  object Bytes:
+    def fromString(s: String): Bytes =
+      s
+        .linesIterator
+        .map:
+          case s"$x,$y" => Pos(x.toInt, y.toInt)
+        .toVector
+
+    extension (self: Bytes)
+      def toExit(start: Pos, exit: Pos, corrupted: Int): Option[(Pos, Int)] =
+        val corruptedBytes = self.take(corrupted)
+        val bound          = Box(start, exit)
+
+        @tailrec def loop(
+          todo: Vector[(Pos, Int)],
+          v: Set[Pos]
+        ): Option[(Pos, Int)] =
+          todo.headOption match
+            case None                        => None
+            case Some(p, d) if p == exit     => Some(p, d)
+            case Some(p, _) if v.contains(p) => loop(todo.tail, v)
+            case Some(p, d) =>
+              val next =
+                p
+                  .axisOffsetsFn: p =>
+                    !corruptedBytes.contains(p) && bound.contains(p)
+                  .map(p => (p, d + 1))
+
+              loop(todo.tail ++ next, v + p)
+
+        loop(Vector((start, 0)), Set.empty[Pos])
+
+      def blocking(start: Pos, exit: Pos): Pos =
+        val i =
+          exponentialBinarySearch(0, Some(bytes.length)): i =>
+            toExit(
+              start = start,
+              exit = exit,
+              corrupted = i
+            ).isDefined
+
+        self(i - 1)
+
+  import Bytes.*
+
+  lazy val pt1: Int =
+    val (_, distance) =
+      bytes
+        .toExit(
+          start = Pos.zero,
+          exit = if getEnv == Test then Pos(6, 6) else Pos(70, 70),
+          corrupted = if getEnv == Test then 12 else 1024
+        )
+        .get
+
+    distance
+
+  lazy val pt2: String =
+    val Pos(x, y) =
+      bytes.blocking(
+        start = Pos.zero,
+        exit = if getEnv == Test then Pos(6, 6) else Pos(70, 70)
+      )
+
+    s"$x,$y"
+
+  answer(1)(pt1)
+
+  answer(2)(pt2)
--- code/utilities/src/main/scala/adventofcode/utilities/Utilities.scala
+++ code/utilities/src/main/scala/adventofcode/utilities/Utilities.scala
@@ -13,12 +13,42 @@ object Utilities:
 
       sys.exit(1)
 
+  /** Looking for the first time that f returns false */
+  @tailrec def binarySearch[A](min: A, max: A)(f: A => Boolean)(using
+    int: Integral[A]
+  ): A =
+    if min == max then min
+    else
+      val mid = (max - min) / int.fromInt(2) + min
+
+      if f(mid) then binarySearch(mid + int.one, max)(f)
+      else binarySearch(min, mid)(f)
+
+  /** Looking for the first range in which f returns false */
+  def exponentialSearch[A](min: A)(f: A => Boolean)(using
+    int: Integral[A]
+  ): (A, A) =
+    @tailrec def loop(lower: A, upper: A): (A, A) =
+      if f(min + upper) then loop(upper, upper * int.fromInt(2))
+      else (min + lower, min + upper)
+
+    loop(int.zero, int.one)
+
+  /** Looking for the first time that f returns false */
+  def exponentialBinarySearch[A](
+    min: A,
+    max: Option[A] = None
+  )(f: A => Boolean)(using int: Integral[A]): A =
+    val (lower, upper) =
+      exponentialSearch(min)(f)
+
+    binarySearch(lower, max.fold(upper)(_ min upper))(f)
+
   def exponentialSearch[A, B](f: A => B, min: A)(x: B)(using
     int: Integral[A],
     ord: Ordering[B]
   ): (A, A) =
-    @annotation.tailrec
-    def loop(lower: A, upper: A): (A, A) =
+    @tailrec def loop(lower: A, upper: A): (A, A) =
       if f(min + upper) >= x then
         (min + lower, min + upper)
       else
@@ -33,8 +63,7 @@ object Utilities:
     int: Integral[A],
     ord: Ordering[B]
   ): A =
-    @annotation.tailrec
-    def loop(lower: A, upper: A): A =
+    @tailrec def loop(lower: A, upper: A): A =
       if lower == upper then
         lower
       else
@@ -72,8 +101,7 @@ object Utilities:
     @targetName("wholeNumberModulo")
     def +%(n: A): A = (a % n + n) % n
 
-    @tailrec
-    def gcd(b: A): A =
+    @tailrec def gcd(b: A): A =
       if b == Integral[A].zero then a.abs else b.gcd(a % b)
 
     def lcm(b: A): A =
