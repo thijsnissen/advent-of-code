6ade19dfa13a62001914c3514ee2d928f7a8bd1a
Thijs Nissen <thijsnissen@gmail.com>
2024-12-20 21:48:25 +0100

    Finished 2024 day 20 (#70)

---
+++ code/2024/src/main/scala/adventofcode/aoc2024/Day20.scala
@@ -0,0 +1,69 @@
+package adventofcode
+package aoc2024
+
+import utilities.AdventOfCode.*
+import utilities.Grid
+import utilities.Grid.*
+import utilities.Pos
+
+object Day20 extends AdventOfCode(Prod):
+  val raceTrack: Grid[Char] =
+    Grid.unit:
+      input.linesIterator.map(_.toVector).toVector
+
+  extension (self: Grid[Char])
+    def next(p: Pos): Set[Pos] =
+      p.axisOffsetsFn: p =>
+        self.lift(p.x, p.y).exists(Set('.', 'S', 'E').contains)
+
+    def race(s: Pos, e: Pos): Map[Pos, Int] =
+      @tailrec def loop(
+        todo: Vector[(Pos, Int)],
+        path: Map[Pos, Int]
+      ): Map[Pos, Int] =
+        todo.head match
+          case (p, d) if p == e => path + (p -> d)
+          case (p, d) if !path.contains(p) =>
+            loop(todo.tail ++ next(p).map(_ -> (d + 1)), path + (p -> d))
+          case _ => loop(todo.tail, path)
+
+      loop(Vector((s, 0)), Map.empty[Pos, Int])
+
+  extension (self: Map[Pos, Int])
+    def cheats(ps: Int): Vector[Int] =
+      @tailrec def loop(todo: Vector[Pos], saved: Vector[Int]): Vector[Int] =
+        todo.headOption match
+          case None => saved
+          case Some(p) =>
+            val c =
+              self
+                .keys
+                .toVector
+                .filter(q => p.manhattan(q) <= ps)
+                .map(q => self(q) - self(p) - p.manhattan(q).toInt)
+
+            loop(todo.tail, saved ++ c)
+
+      loop(self.keys.toVector, Vector.empty[Int])
+
+  lazy val pt1: Int =
+    raceTrack
+      .race(
+        s = raceTrack.findPos('S'),
+        e = raceTrack.findPos('E')
+      )
+      .cheats(ps = 2)
+      .count(_ >= (if getEnv == Test then 20 else 100))
+
+  lazy val pt2: Int =
+    raceTrack
+      .race(
+        s = raceTrack.findPos('S'),
+        e = raceTrack.findPos('E')
+      )
+      .cheats(ps = 20)
+      .count(_ >= (if getEnv == Test then 50 else 100))
+
+  answer(1)(pt1)
+
+  answer(2)(pt2)
